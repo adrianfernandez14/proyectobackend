4f4cda06ae85598d9beb3405901ff0851f192f3c
Adrian Fernandez <adriam.fernandez@live.com>
2024-08-25 13:28:07 -0300

    preparando estructura de mi proyecto

--- src/app.js
+++ src/app.js
@@ -1,16 +1,8 @@
-const express = require("express");
-const app = express();
-const PUERTO = 8080;
-const productRoutes = require('./routes/products.router.js');
-const cartsRoutes = require('./routes/carts.router.js');
-const bodyParser = require("body-parser");
-
-require('dotenv').config();
-
-app.use(bodyParser.json());
-app.use('/api/products', productRoutes);
-app.use('/api/carts', cartsRoutes);
-
-app.listen(PUERTO, () => {
-    console.log('Escuchando en el puerto 8080')
-})
+import mongoose from "mongoose";
+import orderModel from "./models/";
+const main = async() => {
+    mongoose.connect("");
+    const resultado = await orderModel.aggregate([
+        
+    ])
+}
