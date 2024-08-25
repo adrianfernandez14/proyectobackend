0572b90dbe53d04f30902281044235f8f0951b97
Adrian Fernandez <adriam.fernandez@live.com>
2024-08-25 17:10:49 -0300

    aplicando motores de plantilla, y conectandome a mi BD mongodb

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+import express from "express";
+const app = express();
+const PUERTO = 8080;
+import exphds from "express-handlebars";
+
 import mongoose from "mongoose";
 import orderModel from "./models/";
 const main = async() => {
-    mongoose.connect("");
+    mongoose.connect("mongodb+srv://adrianfernandez:coderhouse@cluster0.uetamcp.mongodb.net/Fiambreria");
     const resultado = await orderModel.aggregate([
-        
+
     ])
 }
+
+//Middleware:
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
